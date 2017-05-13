0032a8a6fd2c0bce2dbcf638fd53eb6d0386575f
IVleafclover <blubber1991@gmx.de>
2017-05-13 11:03:19 +0200

    added dialogs in settings

--- app/src/main/java/de/ivleafcloverapps/pillreminder/SettingsFragment.java
+++ app/src/main/java/de/ivleafcloverapps/pillreminder/SettingsFragment.java
@@ -1,22 +1,29 @@
 package de.ivleafcloverapps.pillreminder;
 
+import android.app.DatePickerDialog;
 import android.app.Fragment;
+import android.app.TimePickerDialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.EditText;
-
-import java.util.Date;
+import android.widget.TimePicker;
 
 /**
  * Created by Christian on 11.05.2017.
  */
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
+
+    private final int PERIOD_BEGIN_DIALOG_ID = 0;
+    private final int NOTIFICATION_TIME_DIALOG_ID = 1;
+    private final int NOTIFICATION_PERIOD_DIALOG_ID = 2;
 
     @Nullable
     @Override
@@ -29,8 +36,28 @@ public class SettingsFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         EditText periodBegin = (EditText) getView().findViewById(R.id.editPeriodBegin);
+        periodBegin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               showDialog(PERIOD_BEGIN_DIALOG_ID);
+            }
+        });
+
         EditText notificationTime = (EditText) getView().findViewById(R.id.editNotificationTime);
+        notificationTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(NOTIFICATION_TIME_DIALOG_ID);
+            }
+        });
+
         EditText notificationPeriod = (EditText) getView().findViewById(R.id.editNotificationPeriod);
+        notificationPeriod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(NOTIFICATION_PERIOD_DIALOG_ID);
+            }
+        });
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getView().getContext());
 
@@ -66,5 +93,33 @@ public class SettingsFragment extends Fragment {
             // Nothing to do here
         }
         editor.commit();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Log.d("PillReminder", "chosen date: " + year + " " + month + " " + dayOfMonth);
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+
+    private void showDialog(int id) {
+        switch(id) {
+            case PERIOD_BEGIN_DIALOG_ID:
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getView().getContext(), SettingsFragment.this, 2000 , 10, 1);
+                datePickerDialog.show();
+                break;
+            case NOTIFICATION_TIME_DIALOG_ID:
+                TimePickerDialogWithId dialog = new TimePickerDialogWithId(NOTIFICATION_TIME_DIALOG_ID, getView().getContext(), SettingsFragment.this, 0, 0, true);
+                dialog.show();
+                break;
+            case NOTIFICATION_PERIOD_DIALOG_ID:
+                TimePickerDialogWithId dialog2 = new TimePickerDialogWithId(NOTIFICATION_PERIOD_DIALOG_ID, getView().getContext(), SettingsFragment.this, 0, 0, true);
+                dialog2.show();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/de/ivleafcloverapps/pillreminder/TimePickerDialogWithId.java
@@ -0,0 +1,25 @@
+package de.ivleafcloverapps.pillreminder;
+
+import android.app.TimePickerDialog;
+import android.content.Context;
+
+/**
+ * Created by Christian on 13.05.2017.
+ */
+
+public class TimePickerDialogWithId extends TimePickerDialog {
+    private int id;
+
+    public TimePickerDialogWithId(int id, Context context, OnTimeSetListener listener, int hourOfDay, int minute, boolean is24HourView) {
+        super(context, listener, hourOfDay, minute, is24HourView);
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
